8a33624a6e9447152f28df7d132c6023eba5dbb9
loc-cv <donbb6824@gmail.com>
2023-01-06 12:50:02 +0700

    Add new room form and add some styles for rooms and users

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,7 +1,18 @@
 class RoomsController < ApplicationController
   before_action :authenticate_user!
   def index
+    @room = Room.new # for the new room form in room index view
     @rooms = Room.public_rooms
     @users = User.all_except(current_user)
+  end
+
+  def create
+    @room = Room.create(room_params)
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name)
   end
 end
