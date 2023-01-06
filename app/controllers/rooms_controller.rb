7d1fe53b91471d2491008410df5d95d7ca1752bd
loc-cv <donbb6824@gmail.com>
2023-01-06 16:07:32 +0700

    Finish basic functionalities for sending messages

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,9 +1,14 @@
 class RoomsController < ApplicationController
   before_action :authenticate_user!
-  def index
-    @room = Room.new # for the new room form in room index view
-    @rooms = Room.public_rooms
-    @users = User.all_except(current_user)
+  before_action :set_info, only: %i[index show]
+
+  def index; end
+
+  def show
+    @active_room = Room.find(params[:id])
+    @message = Message.new # for the message input
+    @messages = @active_room.messages.order(created_at: :asc)
+    render :index
   end
 
   def create
@@ -11,6 +16,12 @@ class RoomsController < ApplicationController
   end
 
   private
+
+  def set_info
+    @room = Room.new # for the new room in room index view
+    @public_rooms = Room.public_rooms
+    @other_users = User.all_except(current_user)
+  end
 
   def room_params
     params.require(:room).permit(:name)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  resources :rooms
+  resources :rooms do
+    resources :messages
+  end
 
   devise_for :users
   devise_scope :user do
