435bd6fc6236f7153ee8e05ebb5aae23d1998b14
loc-cv <donbb6824@gmail.com>
2023-01-06 11:15:28 +0700

    Basic setup for rooms and users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  resources :rooms
 
   devise_for :users
   devise_scope :user do
-    get 'users', to: 'devise/sessions#new'
+    get :users, to: 'devise/sessions#new'
   end
+  get 'users/:id', to: 'users#show', as: :user
 end
